058f0031395cae8471e40851b9b1114d38ce5631
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2023-10-11 18:24:33 +1000

    fixed assets path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: 'export',
+	basePath: process.env.NEXT_PUBLIC_LIVE_SITE ? '/token-zen-garden' : '',
 };
 
 module.exports = nextConfig;
